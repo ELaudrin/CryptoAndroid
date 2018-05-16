a911b1ead77595b2a707996dd605d0aa002c3b2a
erwan laudrin <erwan.laudrin@viacesi.fr>
2018-05-16 11:23:34 +0200

    deux page

--- app/src/main/java/com/example/erwan/projetcrypto/MainActivity.java
+++ app/src/main/java/com/example/erwan/projetcrypto/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.erwan.projetcrypto;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import java.util.*;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +17,38 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        android.widget.ListView liste = findViewById(R.id.list);
+        ArrayList maListe = new ArrayList();
+        maListe.add("Bitcoin");
+        maListe.add("Ethereum");
+        maListe.add("Litecoin");
+        maListe.add("Ripple");
+
+
+         ArrayAdapter adt = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, android.R.id.text1, maListe);
+        liste.setAdapter(adt);
+
+
+        liste.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position==0){
+                    /*Toast toast= Toast.makeText(MainActivity.this,"En cours de développement",Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER, 0, 0);toast.show();*/
+
+                    Intent intent0 = new Intent(MainActivity.this, detailCrypto.class);
+                    startActivity(intent0);
+
+                }else{
+
+                    Intent intent1 = new Intent(MainActivity.this, detailCrypto.class);
+                    startActivity(intent1);
+                }
+
+            };
+        });
+
     }
 }
--- app/src/main/java/com/example/erwan/projetcrypto/detailCrypto.java
+++ app/src/main/java/com/example/erwan/projetcrypto/detailCrypto.java
@@ -2,12 +2,15 @@ package com.example.erwan.projetcrypto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
 
-public class detailCrypto extends AppCompatActivity {
+public class detailCrypto extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_crypto);
     }
+
+
 }
