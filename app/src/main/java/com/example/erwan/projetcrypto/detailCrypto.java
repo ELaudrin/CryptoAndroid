8afe7d09cf78751a9d7cfed62a4805300c8d3391
erwan laudrin <erwan.laudrin@viacesi.fr>
2018-05-16 15:12:57 +0200

    listeView

--- app/src/main/java/com/example/erwan/projetcrypto/detailCrypto.java
+++ app/src/main/java/com/example/erwan/projetcrypto/detailCrypto.java
@@ -11,6 +11,50 @@ import android.widget.ListView;
 import android.widget.Toast;
 import java.util.*;
 import android.graphics.*;
+import android.widget.*;
+import android.view.*;
+
+
+class MyAdapter extends BaseAdapter{
+
+    private final Activity context;
+
+    public MyAdapter(Activity mainActivity) {
+        context  = mainActivity;
+    }
+
+    @Override
+    public int getCount() {
+        return 100;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE);
+        View v;
+        if (convertView == null) {
+            v = inflater.inflate(R.layout.adapter_custom, null);
+        } else {
+            v = convertView;
+        }
+        int nb = 1+ (int)(Math.random() * (99999 - 1));
+        TextView texte = v.findViewById(R.id.monTexte);
+        texte.setText("Sold " );
+        TextView texte2 = v.findViewById(R.id.monTexte2);
+        texte2.setText("" + nb );
+        return v;
+    }
+}
 
 public class detailCrypto extends Activity {
 
@@ -19,14 +63,19 @@ public class detailCrypto extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_crypto);
 
+        ListView liste = findViewById(R.id.list);
+        ListAdapter myAdapter = new MyAdapter(this);
+        liste.setAdapter(myAdapter);
 
 
-        android.widget.ListView liste = findViewById(R.id.list);
+
+
+        /*android.widget.ListView liste = findViewById(R.id.list);
 
         java.util.ArrayList maListe = new ArrayList();
-        maListe.add("Bitcoin");
-        maListe.add("Ethereum");
-        maListe.add("Litecoin");
+        maListe.add("Sold");
+        maListe.add("Bought");
+        maListe.add("Sold");
         maListe.add("Ripple");
 
 
@@ -50,8 +99,15 @@ public class detailCrypto extends Activity {
                 }
 
             };
-        });
+        });*/
+
+
+
+
     }
+
+
+
 
 
 }
